5e63c3b9c7dd9ac1d7273a70aeaa878f901bcf72
Dan Carpenter <dan@omadahealth.com>
2018-03-05 09:52:15 -0500

    Document the KNN Regression model

--- lib/lurn/neighbors/knn_regression.rb
+++ lib/lurn/neighbors/knn_regression.rb
@@ -8,11 +8,27 @@ module Lurn
         @k = k
       end
 
+      # Trains the KNN regression model to predict the target variable
+      # based on the predictors. For KNN Regression all computation is
+      # deferred until the time of prediction so in this case the data
+      # is just stored.
+      #
+      # @param predictors [Array-like] An array of arrays containing the predictor data
+      # @param targets [Array-like] An array with the value you want to predict
       def fit(predictors, targets)
         @predictors = predictors.map { |pred| Vector.elements(pred) }
         @targets = targets
+
+        nil
       end
 
+      # Predicts the value of the given observation by averaging the target value of the
+      # closest k predictor observations based on euclidian distance.
+      #
+      # @param vector [Array-like]
+      #   An array (or array-like) of the same length as the predictors used
+      #   to fit the model
+      # @return [Float] The predicted value
       def predict(vector)
         vector = Vector.elements(vector)
 
@@ -24,7 +40,7 @@ module Lurn
 
         neighbors = distances.first(@k).map { |pair| pair[:value] }
 
-        neighbors.sum / neighbors.length
+        neighbors.sum.to_f / neighbors.length.to_f
       end
 
       private
