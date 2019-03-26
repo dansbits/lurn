6113125769c8eeb1d85ff9fa88de1a5f9289bfbc
= <=>
2019-03-26 08:42:06 +0000

    WIP: Rename some variables

--- lib/lurn/structures/kd_tree.rb
+++ lib/lurn/structures/kd_tree.rb
@@ -1,17 +1,17 @@
 module Lurn
   class KDTree
 
-    attr :left_tree
-    attr :right_tree
-    attr :partition_column
-    attr :partition
+    attr_reader :left_tree
+    attr_reader :right_tree
+    attr_reader :partition_dimension
+    attr_reader :partition
+    attr_reader :vector
 
-    def initialize(vectors, partition_column = 0)
-      @partition_column = partition_column
-      @left_tree = nil
-      @right_tree = nil
+    def initialize(vectors, partition_dimension = 0)
+      self.partition_dimension = partition_dimension
+
       if vectors.count == 1
-        @vector = vectors[0]
+        self.vector = vectors[0]
       else
         partition_vectors(vectors)
       end
@@ -20,11 +20,11 @@ module Lurn
     def pretty_print(depth = 0)
       output = ""
       output += "root:\n" if depth == 0
-      output += ("  ") * (depth + 1) + "left_tree: v[#{@partition_column}] <= #{@partition}\n#{@left_tree.pretty_print(depth+1)}" if @left_tree
-      output += ("  ") * (depth + 1) + "right_tree: v[#{@partition_column}] > #{@partition}\n#{@right_tree.pretty_print(depth+1)}" if @right_tree
+      output += ("  ") * (depth + 1) + "left_tree: v[#{partition_dimension}] <= #{partition}\n#{left_tree.pretty_print(depth+1)}" if @left_tree
+      output += ("  ") * (depth + 1) + "right_tree: v[#{@partition_dimension}] > #{partition}\n#{right_tree.pretty_print(depth+1)}" if @right_tree
       if @left_tree.nil? && @right_tree.nil?
-        if @vector
-          output += ("  ") * (depth + 1) + @vector.to_s + "\n"
+        if vector
+          output += ("  ") * (depth + 1) + vector.to_s + "\n"
         else
           output += ("  ") * (depth + 1) + "LEAF NODE"
         end
@@ -34,22 +34,16 @@ module Lurn
     
     private
 
+    vectors.sort! { |v1, v2| v1[partition_dimension] <=> v2[@partition_dimension] }
     def partition_vectors(vectors)
-      vectors.sort! { |v1, v2| v1[@partition_column] <=> v2[@partition_column] }
       median_index = (vectors.length / 2) - 1
-      @partition = vectors[median_index][@partition_column]
+      partition = vectors[median_index][partition_dimension]
 
-      left_vectors, right_vectors = [], []
-      vectors.each do |v|
-        if v[@partition_column] <= @partition
-          left_vectors.push v
-        else
-          right_vectors.push v
-        end
-      end
+      left_vectors = vectors[0..median_index]
+      right_vectors = vectors[(median_index + 1)..-1]
 
-      @left_tree = build_child(left_vectors)
-      @right_tree = build_child(right_vectors)
+      left_tree = build_child(left_vectors)
+      right_tree = build_child(right_vectors)
     end
 
     def build_child(vectors)
